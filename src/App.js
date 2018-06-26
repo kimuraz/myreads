7f5292cee7f83b792982a0f2aff416d46b356c6e
Kimura <joseduardo.kimura@gmail.com>
2018-06-26 01:37:58 -0300

    :bulb: Add book loading to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import BooksApi from './utils/BooksApi';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: null
+    };
+  }
+
+  componentDidMount() {
+    const api = new BooksApi()
+    api.getAll().then(data => {
+      console.log(data);
+      this.setState({ ...this.state, books: data.books });
+    });
+  }
+
   render() {
-    return <div className="App" />;
+    const { books } = this.state
+    return (
+      <div className="App">
+      </div>
+    );
   }
 }
 
